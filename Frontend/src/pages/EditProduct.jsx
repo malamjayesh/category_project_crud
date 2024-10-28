0fe54d8978c9123e745c93efa265e715502d98e9
malamjayesh <151116920+malamjayesh@users.noreply.github.com>
2024-10-28 18:10:58 +0530

    few changes

---
+++ Frontend/src/pages/EditProduct.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function EditProduct() {
+  return (
+    <div className="flex justify-center items-center">
+      <h1 className="text-3xl">Edit Product Page</h1>
+    </div>
+  );
+}
+
+export default EditProduct;
